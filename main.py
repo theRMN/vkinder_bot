92e2ad972b81f4af0da5eccf0c929d29e1c7e6ef
theRMN <roman.vassev@gmail.com>
2021-05-23 19:29:33 +0600

    search usres from sex, city, age and status added

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import vk_api
-from config import group_token
+from config import group_token, app_token
 from vk_api.longpoll import VkLongPoll, VkEventType
 from random import randrange
+from pprint import pprint
+from datetime import date
 
 session = vk_api.VkApi(token=group_token)
+session_2 = vk_api.VkApi(token=app_token)
+
 longpoll = VkLongPoll(session)
 
 
@@ -17,33 +21,117 @@ def write_msg(user_id, message):
 def get_users(user_id):
     data = session.method('users.get',
                           {'user_ids': user_id,
-                           'fields': 'bdate,sex,city,relation'})
+                           'fields': 'bdate,sex,country,city,relation'})
     return data
 
 
-def search_users(age_from, age_to, sex, city, status):
-    data = session.method('users.search',
-                          {'age_from': age_from,
-                           'age_to': age_to,
-                           'sex': sex,
-                           'city': city,
-                           'status': status,
-                           'count': 1})
+def search_users(age=0, sex=0, city=0):
+
+    if sex == 2:
+        sex = 1
+    else:
+        sex = 2
+
+    data = session_2.method('users.search',
+                            {'age_from': age - 1,
+                             'age_to': age + 1,
+                             'sex': sex,
+                             'city': city,
+                             'status': 6,
+                             'count': 5,
+                             'fields': 'photo_max_orig'})
+    pprint(data)
     return data
 
 
-for event in longpoll.listen():
-    if event.type == VkEventType.MESSAGE_NEW:
+def get_city_from_id(country, city):
+    data = session_2.method('database.getCities',
+                            {'country_id': country,
+                             'q': city,
+                             'count': 1})
 
-        if event.to_me:
-            request = event.text.lower()
+    city_id = data['items'][0]['id']
+    return city_id
 
-            if request == '/h':
-                write_msg(event.user_id, f'===== Список комманд =====\n'
-                                         f'/h - помощ\n'
-                                         f'/s - поиск пользователей VK\n')
 
-            elif request == '/s':
-                search_users(19, 25, 1, 144, 1)
-                # get_users('8430489')
-                write_msg(event.user_id, 'ок')
+def send_hello(event):
+    name = get_users(event.user_id)[0]['first_name']
+    write_msg(event.user_id, f'Привет, {name}!\n Для того, чтобы увидеть список комманд введи "/h"')
+    return
+
+
+def send_menu(event):
+    write_msg(event.user_id, f'===== Список комманд =====\n'
+                             f'/p - рандомная картинка\n'
+                             f'/s - поиск\n')
+    return
+
+
+def send_picture(event):
+    write_msg(event.user_id, 'Не, ну не всё сразу....')
+    return
+
+
+def send_search(event):
+    data = {'first_name': None,
+            'id': None,
+            'last_name': None,
+            'sex': None,
+            'country': None,
+            'город': None,
+            'возраст': None}
+
+    for i in get_users(event.user_id)[0].items():
+        data[i[0]] = i[1]
+        if i[0] == 'bdate' and len(i[1].split('.')[-1]) == 4:
+            data['возраст'] = date.today().year - int(i[1].split('.')[-1])
+
+    country_id = get_users(event.user_id)[0]['country']['id']
+
+    for i in data.items():
+        if i[1] is None:
+            write_msg(event.user_id, f'Введите свой {i[0]}')
+
+            for event in longpoll.listen():
+                if event.type == VkEventType.MESSAGE_NEW:
+
+                    if event.to_me:
+                        request = event.text.lower()
+
+                        if i[0] == 'город':
+                            data['город'] = get_city_from_id(country_id, request)
+                        else:
+                            data[i[0]] = request
+                        break
+    pprint(data)
+
+    data_2 = search_users(int(data['возраст']), data['sex'], data['город'])
+
+    first_name = data_2['items'][0]['first_name']
+    last_name = data_2['items'][0]['last_name']
+    link = 'vk.com/id' + str(data_2['items'][0]['id'])
+    photo = data_2['items'][0]['photo_max_orig']
+
+    write_msg(event.user_id, f'{first_name} {last_name} {link}\n{photo}')
+    return
+
+
+def execute():
+    for event in longpoll.listen():
+        if event.type == VkEventType.MESSAGE_NEW:
+
+            if event.to_me:
+                request = event.text.lower()
+
+                if request in ['начать', 'привет']:
+                    send_hello(event)
+                elif request == '/h':
+                    send_menu(event)
+                elif request == '/p':
+                    send_picture(event)
+                elif request == '/s':
+                    send_search(event)
+
+
+if __name__ == '__main__':
+    execute()
